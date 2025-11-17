a9abd937ffd19dd6556790768ab7cd16230271a7
ParadiseFallen <50731756+ParadiseFallen@users.noreply.github.com>
2025-11-17 12:27:56 +0300

    Gallery refactor (#139)
    * [#118] Add blazor routing
    
    * Fix default page for gallery
    
    * [#127] Add nuget.config
    
    * [#136] Refactored gallery

--- src/RazorConsole.Gallery/Program.cs
+++ src/RazorConsole.Gallery/Program.cs
@@ -1,26 +1,21 @@
-using System;
-using System.Runtime.CompilerServices;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using RazorConsole.Components;
+
 using RazorConsole.Core;
-using RazorConsole.Core.Utilities;
 using RazorConsole.Gallery.Components;
 using RazorConsole.Gallery.Services;
-using Spectre.Console;
 
-var builder = Host.CreateDefaultBuilder(args)
-    .UseRazorConsole<App>();
+var builder = Host
+    .CreateApplicationBuilder(args);
 
-builder.ConfigureServices(services =>
+builder.UseRazorConsole<App>();
+
+builder.Services.AddHttpClient<INuGetUpgradeChecker, NuGetUpgradeChecker>(client =>
 {
-    services.AddHttpClient<INuGetUpgradeChecker, NuGetUpgradeChecker>(client =>
-    {
-        client.BaseAddress = new Uri("https://api.nuget.org/v3-flatcontainer/", UriKind.Absolute);
-        client.DefaultRequestHeaders.UserAgent.ParseAdd("RazorConsoleGallery/1.0");
-    });
+    client.BaseAddress = new Uri("https://api.nuget.org/v3-flatcontainer/", UriKind.Absolute);
+    client.DefaultRequestHeaders.UserAgent.ParseAdd("RazorConsoleGallery/1.0");
 });
 
-var host = builder.Build();
-
-await host.RunAsync();
+await builder
+    .Build()
+    .RunAsync();
