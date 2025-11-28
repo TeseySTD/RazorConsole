9ba419ead081007816030f5411c3887e8ff2d160
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2025-11-29 01:17:37 +0200

    feat:Add auto height scrollbar.

---
+++ src/RazorConsole.Core/Vdom/Translators/ScrollableTranslator.cs
@@ -0,0 +1,49 @@
+// Copyright (c) RazorConsole. All rights reserved.
+
+using RazorConsole.Core.Rendering.Renderables;
+using RazorConsole.Core.Rendering.Vdom;
+using Spectre.Console.Rendering;
+
+namespace RazorConsole.Core.Vdom.Translators;
+
+public class ScrollableTranslator : IVdomElementTranslator
+{
+    public int Priority => 30;
+
+    public bool TryTranslate(VNode node, TranslationContext context, out IRenderable? renderable)
+    {
+        renderable = null;
+        if (!string.Equals(node.TagName, "scrollable", StringComparison.OrdinalIgnoreCase))
+        {
+            return false;
+        }
+
+        if (!VdomSpectreTranslator.TryParsePositiveInt(VdomSpectreTranslator.GetAttribute(node, "data-items-count"),
+                out var itemsCount))
+        {
+            return false;
+        }
+
+        if (!int.TryParse(VdomSpectreTranslator.GetAttribute(node, "data-offset"), out var offset))
+        {
+            return false;
+        }
+
+        if (!VdomSpectreTranslator.TryParsePositiveInt(VdomSpectreTranslator.GetAttribute(node, "data-page-size"),
+                out var pageSize))
+        {
+            return false;
+        }
+
+
+        if (!VdomSpectreTranslator.TryConvertChildrenToRenderables(node.Children, context,
+                out var contentRenderable))
+        {
+            return false;
+        }
+
+        renderable = new ScrollableWithBarRenderable(contentRenderable, itemsCount, offset, pageSize);
+
+        return true;
+    }
+}
