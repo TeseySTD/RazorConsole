3f8628045b4d3c2a555c4d904a0486f2b0e6e9bf
Skoreyko Misha <150385054+TeseySTD@users.noreply.github.com>
2025-12-04 22:46:19 +0200

    feat(website):Add wasm lazy-loading and LoadingOverlay component. (#238)

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { lazy, Suspense } from "react"
 import Layout from "@/components/Layout"
+import LoadingOverlay from "@/components/LoadingOverlay"
+import { useThemeEffect } from "@/components/ThemeProvider"
+import ScrollToTop from "@/components/ScrollToTop"
+
+// --- Eager Imports ---
 import Home from "@/pages/Home"
 import Docs from "@/pages/Docs"
 import QuickStart from "@/pages/QuickStart"
-import ComponentsLayout from "@/pages/components/Layout"
-import ComponentsOverview from "@/pages/components/Overview"
-import ComponentDetail from "@/pages/components/Detail"
 import Advanced from "@/pages/Advanced"
 import Collaborators from "@/pages/Collaborators"
-import { useThemeEffect } from "@/components/ThemeProvider"
-import ScrollToTop from "@/components/ScrollToTop"
+
+// --- Lazy Imports ---
+const ComponentsLayout = lazy(() => import("@/pages/components/Layout"))
+const ComponentsOverview = lazy(() => import("@/pages/components/Overview"))
+const ComponentDetail = lazy(() => import("@/pages/components/Detail"))
 
 function App() {
   useThemeEffect()
@@ -22,10 +28,16 @@ function App() {
           <Route index element={<Home />} />
           <Route path="docs/:topicId?" element={<Docs />} />
           <Route path="quick-start" element={<QuickStart />} />
-          <Route path="components" element={<ComponentsLayout />}>
+
+          <Route path="components" element={
+            <Suspense fallback={<LoadingOverlay text={"Importing WASM runtime"} />}>
+              <ComponentsLayout />
+            </Suspense>
+          }>
             <Route index element={<ComponentsOverview />} />
             <Route path=":name" element={<ComponentDetail />} />
           </Route>
+
           <Route path="advanced" element={<Advanced />} />
           <Route path="collaborators" element={<Collaborators />} />
         </Route>
