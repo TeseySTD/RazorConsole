3ca52a81ce6f44650253eb435ddc9af220a05cec
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2025-12-24 00:38:42 +0200

    refactor:Change absolute positioning to correct fixed.

--- src/RazorConsole.Core/Rendering/Translation/Translators/FixedPositionMiddleware.cs
+++ src/RazorConsole.Core/Rendering/Translation/Translators/FixedPositionMiddleware.cs
@@ -9,13 +9,13 @@ using Spectre.Console.Rendering;
 
 namespace RazorConsole.Core.Rendering.Translation.Translators;
 
-public class AbsolutePositionMiddleware : ITranslationMiddleware
+public class FixedPositionMiddleware : ITranslationMiddleware
 {
     public IRenderable Translate(TranslationContext context, TranslationDelegate next, VNode node)
     {
         var position = VdomSpectreTranslator.GetAttribute(node, "position");
 
-        if (!string.Equals(position, "absolute", StringComparison.OrdinalIgnoreCase))
+        if (!string.Equals(position, "fixed", StringComparison.OrdinalIgnoreCase))
         {
             return next(node);
         }
