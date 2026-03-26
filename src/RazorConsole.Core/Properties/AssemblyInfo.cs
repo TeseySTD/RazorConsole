b0b693d56013e88b404b41a922691b5a147d6862
Max <93036397+Abaddax@users.noreply.github.com>
2026-03-26 09:41:45 +0100

    Fixed typo 'InternalsVisibleTo' (#318)
    Co-authored-by: Xiaoyun Zhang <bigmiao.zhang@gmail.com>

--- src/RazorConsole.Core/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) RazorConsole. All rights reserved.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("RazorConsole.Tests")]
-[assembly: InternalsVisibleTo("RazorConsole.Website")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
