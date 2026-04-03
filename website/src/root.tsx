93d882361cdfc83b1187be0281dd6d4f7009dbad
TeseySTD <skoreykomisha499@gmail.com>
2026-04-03 22:51:31 +0300

    chore:Add gsc auth metatag

--- website/src/root.tsx
+++ website/src/root.tsx
@@ -43,6 +43,7 @@ export default function Root() {
         />
         <link rel="icon" type="image/svg+xml" href={`${baseUrl}razorconsole-icon.svg`} />
         <meta charSet="UTF-8" />
+        <meta name="google-site-verification" content="jF1dcSGbDQJm6UY_MriNs2wHdnEGr_M1wZKiVciIdf8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://api.github.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://api.github.com" />
